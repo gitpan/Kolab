4e5b15e9aae46fc56229be8124da781ec1e91b01
Stephan Buys <sbproxy@icon.co.za>
2004-01-19 12:21:34 +0000

    Import of STEPHANB/Kolab-0.9 from CPAN.
    gitpan-cpan-distribution: Kolab
    gitpan-cpan-version:      0.9
    gitpan-cpan-path:         STEPHANB/Kolab-0.9.tar.gz
    gitpan-cpan-author:       STEPHANB
    gitpan-cpan-maturity:     released

--- Kolab.pm
+++ Kolab.pm
@@ -4,12 +4,6 @@ package Kolab;
 ##  Copyright (c) 2003  Code Fusion cc
 ##
 ##    Writen by Stuart Bingë  <s.binge@codefusion.co.za>
-##    Portions based on work by the following people:
-##
-##      (c) 2003  Tassilo Erlewein  <tassilo.erlewein@erfrakon.de>
-##      (c) 2003  Martin Konold     <martin.konold@erfrakon.de>
-##      (c) 2003  Achim Frank       <achim.frank@erfrakon.de>
-##
 ##
 ##  This  program is free  software; you can redistribute  it and/or
 ##  modify it  under the terms of the GNU  General Public License as
@@ -32,6 +26,7 @@ use Sys::Syslog;
 use URI;
 use Net::LDAP;
 use Kolab::Util;
+#use Kolab::LDAP;
 use vars qw(%config %haschanged);
 
 require Exporter;
@@ -44,6 +39,7 @@ our %EXPORT_TAGS = (
         &reloadConfig
         &reload
         &log
+        &superLog
     ) ]
 );
 
@@ -57,7 +53,7 @@ our @EXPORT = qw(
     &KOLAB_DEBUG
 );
 
-our $VERSION = '0.01';
+our $VERSION = '0.9';
 
 sub KOLAB_SILENT()      { 0 }
 sub KOLAB_ERROR()       { 1 }
@@ -68,16 +64,7 @@ sub KOLAB_DEBUG()       { 4 }
 sub reloadConfig
 {
     my $tempval;
-
-    # Determine the root of the kolab installation, and read `kolab.conf'
-    if (!($tempval = (getpwnam('kolab'))[7])) {
-        $config{'log_level'} = KOLAB_INFO;
-        &log('C', 'Unable to determine the kolab root directory', KOLAB_ERROR);
-        exit(1);
-    } else {
-        %config = readConfig("$tempval/etc/kolab/kolab.conf");
-        $config{'prefix'} = $tempval;
-    }
+    my $ldap;
 
     # `log_level' specifies what severity of messages we want to see in the logs.
     #   Possible values are:
@@ -86,71 +73,101 @@ sub reloadConfig
     #     2 - Warnings & Errors
     #     3 - Info, Warnings & Errors       (DEFAULT)
     #     4 - Debug (i.e. everything)
-    $config{'log_level'} = KOLAB_INFO if (!$config{'log_level'});
+
+    # Determine the root of the kolab installation, and read `kolab.globals'
+    if (!($tempval = (getpwnam('kolab'))[7])) {
+        $config{'log_level'} = KOLAB_WARN;
+        &log('C', 'Unable to determine the kolab root directory', KOLAB_ERROR);
+#        exit(1);
+    } else {
+        %config = readConfig(%config, "$tempval/etc/kolab/kolab.globals");
+        $config{'prefix'} = $tempval;
+    }
+
+    # Now read `kolab.conf', overwriting values read from `kolab.globals'
+    %config = readConfig(\%config, "$tempval/etc/kolab/kolab.conf");
+
+#    $config{'log_level'} = KOLAB_WARN if (!exists $config{'log_level'});
     &log('C', 'Reloading configuration');
 
     # Get the UID/GID of the `kolab' user
     if (!($config{'kolab_uid'} = (getpwnam('kolab'))[2])) {
         &log('C', "Unable to determine the uid of user `kolab'", KOLAB_ERROR);
-        exit(1);
+#        exit(1);
     }
     if (!($config{'kolab_gid'} = (getgrnam('kolab'))[2])) {
         &log('C', "Unable to determine the gid of user `kolab'", KOLAB_ERROR);
-        exit(1);
+#        exit(1);
     }
 
     # Make sure the critical variables we need were defined in kolab.conf
-    if (!$config{'bind_dn'} || !$config{'bind_pw'} || !$config{'ldap_uri'} || !$config{'base_dn'}) {
+    if (!exists $config{'bind_dn'} || !exists $config{'bind_pw'} || !exists $config{'ldap_uri'} || !exists $config{'base_dn'}) {
         &log('C', "One or more required configuration variables (`bind_dn', `bind_pw', `ldap_uri' and/or `base_dn') are missing in `kolab.conf'", KOLAB_ERROR);
-        exit(1);
+#        exit(1);
     }
 
     # Retrieve the LDAP values of the main kolab object to complete our config hash
     if (!($tempval = URI->new($config{'ldap_uri'}))) {
         &log('C', "Unable to parse ldap_uri `" . $config{'ldap_uri'} . "'", KOLAB_ERROR);
-        exit(1);
+#        exit(1);
     } else {
         $config{'ldap_ip'} = $tempval->host;
         $config{'ldap_port'} = $tempval->port;
     }
 
-    my $ldap;
-    my $mesg;
-    my $ldapobject;
-
-    if (!($ldap = Net::LDAP->new($config{'ldap_ip'}, port => $config{'ldap_port'}))) {
-        &log('C', "Unable to connect to LDAP server `" . $config{'ldap_ip'} . ":" . $config{'ldap_port'} . "'", KOLAB_ERROR);
-        exit(1);
-    }
-
-    $mesg = $ldap->bind($config{'bind_dn'}, password => $config{'bind_pw'});
-    if ($mesg->code) {
-        &log('C', "Unable to bind to DN `" . $config{'bind_dn'} . "'", KOLAB_ERROR);
-        exit(1);
-    }
-
     # `kolab_dn' points to the main kolab object in LDAP
     #   Defaults to `k=kolab,$base_dn' if not specified (for backwards compatibility)
-    $config{'kolab_dn'} = "k=kolab," . $config{'base_dn'} if (!$config{'kolab_dn'});
-    if (!($mesg = $ldap->search(
-        base    => $config{'kolab_dn'},
-        scope   => 'base',
-        filter  => '(objectclass=*)'
-    ))) {
-        &log('C', "Unable to find kolab object `" . $config{'kolab_dn'} . "'", KOLAB_ERROR);
-        exit(1);
+    $config{'kolab_dn'} = "k=kolab," . $config{'base_dn'} if (!exists $config{'kolab_dn'});
+    if ($config{'kolab_dn'} eq '') {
+        &log('C', "`kolab_dn' is empty; skipping LDAP read");
+    } else {
+        my $mesg;
+        my $ldapobject;
+
+        if (!($ldap = Net::LDAP->new($config{'ldap_ip'}, port => $config{'ldap_port'}))) {
+            &log('C', "Unable to connect to LDAP server `" . $config{'ldap_ip'} . ":" . $config{'ldap_port'} . "'", KOLAB_ERROR);
+#            exit(1);
+        }
+
+        $mesg = $ldap->bind($config{'bind_dn'}, password => $config{'bind_pw'}) if $ldap;
+        if ($ldap && $mesg->code) {
+            &log('C', "Unable to bind to DN `" . $config{'bind_dn'} . "'", KOLAB_ERROR);
+#            exit(1);
+        }
+
+        #$ldap = Kolab::LDAP::create(
+        #    $config{'ldap_ip'},
+        #    $config{'ldap_port'},
+        #    $config{'bind_dn'},
+        #    $config{'bind_pw'},
+        #    1
+        #);
+        if ($ldap) {
+            $mesg = $ldap->search(
+                base    => $config{'kolab_dn'},
+                scope   => 'base',
+                filter  => '(objectclass=*)'
+            );
+            if (!$mesg->code) {
+                $ldapobject = $mesg->pop_entry;
+                foreach $tempval ($ldapobject->attributes) {
+                    $config{$tempval} = $ldapobject->get_value($tempval);
+                }
+            } else {
+                &log('C', "Unable to find kolab object `" . $config{'kolab_dn'} . "'", KOLAB_ERROR);
+#                exit(1);
+            }
+        } else {
+            &log('C', "Unable to read configuration data from LDAP", KOLAB_WARN);
+        }
     }
 
-    $ldapobject = $mesg->pop_entry;
-    foreach $tempval ($ldapobject->attributes) {
-        $config{$tempval} = $ldapobject->get_value($tempval);
-    }
     # At this point we have read in all user-specified configuration variables.
     # We now need to go through the list of all possible configuration variables
     # and set the default values of those that were not overridden.
 
     # ProFTPd password
-    if ($config{'proftpd-userPassword'}) {
+    if (exists $config{'proftpd-userPassword'}) {
         my $salt = substr($config{'proftpd-userPassword'}, 0, 2);
         $config{'proftpd-userPassword'} = crypt($config{'proftpd-userPassword'}, $salt);
     } else {
@@ -159,16 +176,16 @@ sub reloadConfig
 
     # Apache legacy mode
     $config{'legacy-mode'} = "# no legacy configuration";
-    if ($config{'apache-http'} =~ /true/i) {
+    if (exists $config{'apache-http'} && $config{'apache-http'} =~ /true/i) {
         $config{'legacy-mode'} = 'Include "' . $config{'prefix'} . '/etc/apache/legacy.conf"';
     }
-    $config{'fqdn'} = trim(`hostname -f`);
+    $config{'fqdn'} = trim(`hostname`);
 
     # Cyrus admin account
     $tempval = $config{'cyrus-admins'} || 'manager';
     (my $cmanager, my $dummy) = split(/ /, $tempval, 2);
-    $config{'cyrus_admin'} = $cmanager if (!$config{'cyrus_admin'});
-    $config{'cyrus_admin_pw'} = $config{'bind_pw'} if (!$config{'cyrus_admin_pw'});
+    $config{'cyrus_admin'} = $cmanager if (!exists $config{'cyrus_admin'});
+    $config{'cyrus_admin_pw'} = $config{'bind_pw'} if (!exists $config{'cyrus_admin_pw'});
 
     # `directory_mode' specifies what backend to use (for the main kolab
     # object - for the other objects see their respective XXX_directory_mode).
@@ -182,17 +199,17 @@ sub reloadConfig
     #
     #   Currently supported backends:
     #     `ad' - Active Directory
-    $config{'directory_mode'} = 'slurpd' if (!$config{'directory_mode'});
+#    $config{'directory_mode'} = 'slurpd' if (!exists $config{'directory_mode'});
 
     # `conn_refresh_period' specifies how many minutes to wait before forceably
     # tearing down the change listener connection, re-syncing, and re-connecting.
     # Used by the AD backend.
     # Defaults to one hour.
-    $config{'conn_refresh_period'} = 60 if (!$config{'conn_refresh_period'});
+#    $config{'conn_refresh_period'} = 60 if (!exists $config{'conn_refresh_period'});
 
     # `slurpd_port' specifies what port the kolab slurpd replication daemon listens on
     # Defaults to 9999 for backwards compatibility
-    $config{'slurpd_port'} = 9999 if (!$config{'slurpd_port'});
+#    $config{'slurpd_port'} = 9999 if (!exists $config{'slurpd_port'});
 
     # `user_ldap_uri', `user_bind_dn', `user_bind_pw' and `user_dn_list' are
     # used to specify the DNs where user objects are located. They default to
@@ -206,24 +223,24 @@ sub reloadConfig
     #
     #   TODO: Check Postfix LDAP aliasing when user_dn_list contains more than
     #   one DN.
-    $config{'user_ldap_uri'} = $config{'ldap_uri'} if (!$config{'user_ldap_uri'});
+    $config{'user_ldap_uri'} = $config{'ldap_uri'} if (!exists $config{'user_ldap_uri'});
 
     if (!($tempval = URI->new($config{'user_ldap_uri'}))) {
         &log('C', "Unable to parse user_ldap_uri `" . $config{'user_ldap_uri'} . "'", KOLAB_ERROR);
-        exit(1);
+#        exit(1);
     } else {
         $config{'user_ldap_ip'} = $tempval->host;
         $config{'user_ldap_port'} = $tempval->port;
     }
 
-    $config{'user_bind_dn'} = $config{'bind_dn'} if (!$config{'user_bind_dn'});
-    $config{'user_bind_pw'} = $config{'bind_pw'} if (!$config{'user_bind_pw'});
-    $config{'user_dn_list'} = $config{'base_dn'} if (!$config{'user_dn_list'});
-    $config{'user_directory_mode'} = 'slurpd' if (!$config{'user_directory_mode'});
+    $config{'user_bind_dn'} = $config{'bind_dn'} if (!exists $config{'user_bind_dn'});
+    $config{'user_bind_pw'} = $config{'bind_pw'} if (!exists $config{'user_bind_pw'});
+    $config{'user_dn_list'} = $config{'base_dn'} if (!exists $config{'user_dn_list'});
+    $config{'user_directory_mode'} = $config{'directory_mode'} if (!exists $config{'user_directory_mode'});
 
     # `user_object_class' denotes what object class to search for when locating users.
     # Defaults to `inetOrgPerson'
-    $config{'user_object_class'} = 'inetOrgPerson' if (!$config{'user_object_class'});
+#    $config{'user_object_class'} = 'inetOrgPerson' if (!exists $config{'user_object_class'});
 
     # This part sets various backend-specific LDAP fields (if they have not been
     # overridden) based on `directory_mode'.
@@ -232,59 +249,65 @@ sub reloadConfig
     # `user_field_modified' is used to test whether a user object has been modified
     # `user_field_guid' indicates a field that can be considered globally unique to the object
     # `user_field_quota' indicates a field that stores the cyrus quota for the user
-    if ($config{'user_directory_mode'} eq 'ad') {
-        # AD
-        $config{'user_field_deleted'} = 'isDeleted' if (!$config{'user_field_deleted'});
-        $config{'user_field_modified'} = 'whenChanged' if (!$config{'user_field_modified'});
-        $config{'user_field_guid'} = 'objectGUID' if (!$config{'user_field_guid'});
-        $config{'user_field_quota'} = 'userquota' if (!$config{'user_field_quota'});
-    } else {
-        # slurd/default
-        $config{'user_field_deleted'} = 'deleteflag' if (!$config{'user_field_deleted'});
-        $config{'user_field_modified'} = 'modifytimestamp' if (!$config{'user_field_modified'});
-        $config{'user_field_guid'} = 'entryUUID' if (!$config{'user_field_guid'}); # TODO: Change this, as it is incorrect
-        $config{'user_field_quota'} = 'userquota' if (!$config{'user_field_quota'});
-    }
+#    if ($config{'user_directory_mode'} eq 'ad') {
+#        # AD
+#        $config{'user_field_deleted'} = 'isDeleted' if (!exists $config{'user_field_deleted'});
+#        $config{'user_field_modified'} = 'whenChanged' if (!exists $config{'user_field_modified'});
+#        $config{'user_field_guid'} = 'objectGUID' if (!exists $config{'user_field_guid'});
+#        $config{'user_field_quota'} = 'userquota' if (!exists $config{'user_field_quota'});
+#    } else {
+#        # slurd/default
+#        $config{'user_field_deleted'} = 'deleteflag' if (!exists $config{'user_field_deleted'});
+#        $config{'user_field_modified'} = 'modifytimestamp' if (!exists $config{'user_field_modified'});
+#        $config{'user_field_guid'} = 'entryUUID' if (!exists $config{'user_field_guid'});
+#        $config{'user_field_quota'} = 'userquota' if (!exists $config{'user_field_quota'});
+#    }
 
     # The `sf_XXX' variables are the shared folder equivalents of the `user_XXX' variables
-    $config{'sf_ldap_uri'} = $config{'ldap_uri'} if (!$config{'sf_ldap_uri'});
+    $config{'sf_ldap_uri'} = $config{'ldap_uri'} if (!exists $config{'sf_ldap_uri'});
 
     if (!($tempval = URI->new($config{'sf_ldap_uri'}))) {
         &log('C', "Unable to parse sf_ldap_uri `" . $config{'sf_ldap_uri'} . "'", KOLAB_ERROR);
-        exit(1);
+#        exit(1);
     } else {
         $config{'sf_ldap_ip'} = $tempval->host;
         $config{'sf_ldap_port'} = $tempval->port;
     }
 
-    $config{'sf_bind_dn'} = $config{'bind_dn'} if (!$config{'sf_bind_dn'});
-    $config{'sf_bind_pw'} = $config{'bind_pw'} if (!$config{'sf_bind_pw'});
-    $config{'sf_dn_list'} = $config{'base_dn'} if (!$config{'sf_dn_list'});
-    $config{'sf_directory_mode'} = 'slurpd' if (!$config{'sf_directory_mode'});
+    $config{'sf_bind_dn'} = $config{'bind_dn'} if (!exists $config{'sf_bind_dn'});
+    $config{'sf_bind_pw'} = $config{'bind_pw'} if (!exists $config{'sf_bind_pw'});
+    $config{'sf_dn_list'} = $config{'base_dn'} if (!exists $config{'sf_dn_list'});
+    $config{'sf_directory_mode'} = $config{'directory_mode'} if (!exists $config{'sf_directory_mode'});
 
-    $config{'sf_object_class'} = 'sharedfolder' if (!$config{'sf_object_class'});
+#    $config{'sf_object_class'} = 'sharedfolder' if (!exists $config{'sf_object_class'});
 
-    if ($config{'sf_directory_mode'} eq 'ad') {
-        # AD
-        $config{'sf_field_deleted'} = 'isDeleted' if (!$config{'sf_field_deleted'});
-        $config{'sf_field_modified'} = 'whenChanged' if (!$config{'sf_field_modified'});
-        $config{'sf_field_guid'} = 'entryUUID' if (!$config{'sf_field_guid'});
-        $config{'sf_field_quota'} = 'userquota' if (!$config{'sf_field_quota'});
-    } else {
-        # slurd/default
-        $config{'sf_field_deleted'} = 'deleteflag' if (!$config{'sf_field_deleted'});
-        $config{'sf_field_modified'} = 'modifytimestamp' if (!$config{'sf_field_modified'});
-        $config{'sf_field_guid'} = 'objectGUID' if (!$config{'sf_field_guid'}); # TODO: Change this, as it is incorrect
-        $config{'sf_field_quota'} = 'userquota' if (!$config{'sf_field_quota'});
-    }
+#    if ($config{'sf_directory_mode'} eq 'ad') {
+#        # AD
+#        $config{'sf_field_deleted'} = 'isDeleted' if (!exists $config{'sf_field_deleted'});
+#        $config{'sf_field_modified'} = 'whenChanged' if (!exists $config{'sf_field_modified'});
+#        $config{'sf_field_guid'} = 'entryUUID' if (!exists $config{'sf_field_guid'});
+#        $config{'sf_field_quota'} = 'userquota' if (!exists $config{'sf_field_quota'});
+#    } else {
+#        # slurd/default
+#        $config{'sf_field_deleted'} = 'deleteflag' if (!exists $config{'sf_field_deleted'});
+#        $config{'sf_field_modified'} = 'modifytimestamp' if (!exists $config{'sf_field_modified'});
+#        $config{'sf_field_guid'} = 'entryUUID' if (!exists $config{'sf_field_guid'});
+#        $config{'sf_field_quota'} = 'userquota' if (!exists $config{'sf_field_quota'});
+#    }
 
     # `gyard_deletion_period' specifies how many minutes to leave lost users in
     # the graveyard before deleting them.
     # Defaults to seven days.
-    $config{'gyard_deletion_period'} = 7 * 24 * 60 if (!$config{'gyard_deletion_period'});
+#    $config{'gyard_deletion_period'} = 7 * 24 * 60 if (!exists $config{'gyard_deletion_period'});
+
+    $config{'dirserv_home_server'} = $config{'fqdn'} if (!exists $config{'dirserv_home_server'});
 
     # That's it! We now have our config hash.
-    $ldap->unbind;
+    #Kolab::LDAP::destroy($ldap);
+    if (defined($ldap) && $ldap->isa('Net::LDAP')) {
+        $ldap->unbind;
+        $ldap->disconnect;
+    }
 
     &log('C', 'Finished reloading configuration');
 }
@@ -356,6 +379,12 @@ sub log
     }
 }
 
+sub superLog
+{
+    my $text = shift;
+    syslog('info', "$text");
+}
+
 reloadConfig();
 
 1;
@@ -364,35 +393,12 @@ __END__
 
 =head1 NAME
 
-Kolab - Perl extension for blah blah blah
-
-=head1 SYNOPSIS
-
-  use Kolab;
-  blah blah blah
+Kolab - Perl extension for general Kolab settings.
 
 =head1 ABSTRACT
 
-  This should be the abstract for Kolab.
-  The abstract is used when making PPD (Perl Package Description) files.
-  If you don't want an ABSTRACT you should also edit Makefile.PL to
-  remove the ABSTRACT_FROM option.
-
-=head1 DESCRIPTION
-
-Stub documentation for Kolab, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
-=head2 EXPORT
-
-None by default.
-
-=head1 SEE ALSO
-
-Kolab, Kolab::LDAP
+  Kolab contains code used for loading the configuration values from
+  kolab.conf and LDAP, as well as functions for logging.
 
 =head1 AUTHOR
 
@@ -401,12 +407,6 @@ Stuart Bingë, E<lt>s.binge@codefusion.co.zaE<gt>
 =head1 COPYRIGHT AND LICENSE
 
 Copyright (c) 2003  Code Fusion cc
-
-Portions based on work by the following people:
-
-  (c) 2003  Tassilo Erlewein  <tassilo.erlewein@erfrakon.de>
-  (c) 2003  Martin Konold     <martin.konold@erfrakon.de>
-  (c) 2003  Achim Frank       <achim.frank@erfrakon.de>
 
 This  program is free  software; you can redistribute  it and/or
 modify it  under the terms of the GNU  General Public License as
